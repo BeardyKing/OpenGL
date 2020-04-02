a9fb23f9a7f656defacd7e585864fbbf8c72fb71
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2020-04-02 20:46:38 +0100

    Added VertexBuffer, Vertex Attributes and can now compile vertex and fragment shaders
    currenly showing a red triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,21 +2,60 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main(void){
 
- 
-    
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if (result == GL_FALSE){
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)malloc(length * sizeof(char)); // could be alloca
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "FAILED TO COMPILE " << (type == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        free(message);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+
+int main(void){
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
-
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window){
         glfwTerminate();
         return -1;
     }
@@ -25,12 +64,49 @@ int main(void){
     glfwMakeContextCurrent(window);
 
     if (glewInit() != GLEW_OK) {
-        std::cout << "Err" << std::endl;
         return -1;
     }
-
     std::cout << glGetString(GL_VERSION) << std::endl;
     
+    //vertex positions
+    float positions[6] = {
+        -0.5f, -0.5f,
+        -0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    // Vertex Buffer // buffer of memory that is stored on the GPU
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions, GL_STATIC_DRAW);
+
+    // vertext Attributes // layout of the buffer
+    glEnableVertexAttribArray(0);// enable index 0
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,0);
+
+    std::string vertextShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "gl_Position = position; \n"
+        "}\n";
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "color = vec4(1.0,0.0,0.0,1.0);"
+        "}\n";
+    unsigned int shader = CreateShader(vertextShader, fragmentShader);
+    glUseProgram(shader);
+
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -38,14 +114,7 @@ int main(void){
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(-0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-
-
-
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -54,7 +123,10 @@ int main(void){
         glfwPollEvents();
     }
 
+    glDeleteProgram(shader);
+
     glfwTerminate();
     std::cin.get();
     return 0;
+
 }
