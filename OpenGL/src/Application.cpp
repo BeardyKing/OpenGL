be1dc53e844b093dc1855a700b6f8c15ffac0698
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2020-05-31 22:42:16 +0100

    added imGUI 1.76 library
    added lib and attached MVP to the gui to allow for the camera to be moved

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -18,6 +18,10 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 
 int main(void){
     GLFWwindow* window;
@@ -48,13 +52,45 @@ int main(void){
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+#if __APPLE__
+	// GL 3.2 + GLSL 150
+	const char* glsl_version = "#version 150";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+	// GL 3.0 + GLSL 130
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+	// Setup IMGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+
     { // create scope
         //vertex positions
         float positions[] = {
-             100.0f,  100.0f, 0.0f, 0.0f,//0
-             200.0f,  100.0f, 1.0f, 0.0f,//1
-             200.0f,  200.0f, 1.0f, 1.0f,//2
-             100.0f,  200.0f, 0.0f, 1.0f//3
+             100.0f,  0.0f, 0.0f, 0.0f,//0
+             200.0f,  0.0f, 1.0f, 0.0f,//1
+             200.0f,  100.0f, 1.0f, 1.0f,//2
+             100.0f,  100.0f, 0.0f, 1.0f//3
         };
 
         //index buffer of vertex positions
@@ -81,13 +117,13 @@ int main(void){
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+       /* glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-        glm::mat4 mvp = proj * view * model;
+        glm::mat4 mvp = proj * view * model;*/
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
-        shader.SetUniformsMat4f("u_MVP", mvp);
+        //shader.SetUniformsMat4f("u_MVP", mvp);
         shader.SetUniforms4f("u_Color", 0.8f, 0.2f, 0.5f, 1.0f);
 
         Texture texture("res/textures/fg.png");
@@ -104,14 +140,46 @@ int main(void){
         float r = 0.0f;
         float increment = 0.05f;
 
+        glm::vec3 translation(0, 0, 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window)){
+			glfwPollEvents();
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+
+			ImGui::NewFrame();
+
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("TRANSFROM");                          
+                ImGui::SliderFloat3("translation", &translation.x, 0.0f, 960.0f);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+
+
             /* Render here */
             renderer.Clear();
 
+
+			glm::mat4 model = glm::translate(glm::mat4(1.0f),translation);
+			glm::mat4 mvp = proj * view * model;
+
             shader.Bind();
             shader.SetUniforms4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			shader.SetUniformsMat4f("u_MVP", mvp);
+
             renderer.Draw(va, ib, shader);
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 
             // change to bind shader -> bind vertex array -> index buffer -> issue draw call
 
@@ -127,8 +195,16 @@ int main(void){
             glfwSwapBuffers(window);
             /* Poll for and process events */
             glfwPollEvents();
+
         } 
     }
+
+	// Shutdown
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+
+	ImGui::DestroyContext();
+
     glfwTerminate();
     return 0;
 }
